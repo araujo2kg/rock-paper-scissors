6bd62fb2b0a7877c12cc8fd5103aaee8f0c3c294
Renan S. Araujo <araujo2kg@gmail.com>
2024-11-01 20:04:35 -0300

    Create playRound and playGame functions

--- script.js
+++ script.js
@@ -13,3 +13,44 @@ function getHumanChoice() {
     } while (!CHOICES.includes(choice));
     return choice;
 }
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let round = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log("Draw!");
+        } else if (
+            (humanChoice == "rock" && computerChoice == "scissors") ||
+            (humanChoice == "paper" && computerChoice == "rock") ||
+            (humanChoice == "scissors" && computerChoice == "paper")
+        ) {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+            humanScore++;
+        } else {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+            computerScore++;
+        }
+    }
+
+    while (round < 5) {
+        console.log(`Round ${++round}!`);
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    if (humanScore > computerScore) {
+        console.log(
+            `Game won!  Human:${humanScore} - ${computerScore}:Computer`
+        );
+    } else if (computerScore > humanScore) {
+        console.log(
+            `Game lost! Human:${humanScore} - ${computerScore}:Computer`
+        );
+    } else {
+        console.log(`Draw! Human:${humanScore} - ${computerScore}:Computer`);
+    }
+}
+
+playGame();
